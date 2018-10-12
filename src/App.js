a31acaaec0f889a5145f978e415de7f93dbdee78
Brian Soetens <brian40@live.nl>
2018-10-12 09:17:30 +0200

    finished part 6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import { Header } from "./components/Header";
-import { Body, Body2 } from "./components/Body";
 import "./App.css";
-import Counter from "./components/Counter";
+import ImageSlider from "./components/ImageSlider";
 
 // const Body = () => (
 //   <p className="App-intro">
@@ -18,24 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header
-          title="Hello from App"
-          num={5}
-          myArr={[1, 2, 3]}
-          myFunc={this.add}
-          myObj={{
-            a: 5,
-            b: 6
-          }}
-        />
-        <Body2 />
-        <Body
-          myFunc={this.add}
-          text="this is working as it should"
-          text2="no it doesn't"
-        />
-        <Counter initialCount={0} />
-        <Counter initialCount={100} />
+        <ImageSlider />
       </div>
     );
   }
